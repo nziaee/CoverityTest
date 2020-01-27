0e43f4f3d358043bdb7744f01f1ec002f129d8f3
CORP\nz185011 <nz185011@ncr.com>
2020-01-27 14:55:45 -0600

    Add a console app and class libarary to the project.

---
+++ CoverityTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using TestLibrary;
+
+namespace CoverityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new Class1();
+            user.Name = "Test";
+            Console.WriteLine("Hello "+user.Name+"!");
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args);
+    }
+}
---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class Class1
+    {
+        private string _name;
+
+        public string Name { get; set; }
+    }
+}
